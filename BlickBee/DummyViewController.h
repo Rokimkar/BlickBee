e6ef9330711a2bc9028c334ead1ceb12a342d500
Sanchit Kumar Singh <Rokimkar@Sanchits-MacBook-Pro.local>
2015-11-15 14:56:43 +0530

    dummy

---
+++ BlickBee/DummyViewController.h
@@ -0,0 +1,13 @@
+//
+//  DummyViewController.h
+//  
+//
+//  Created by Sanchit Kumar Singh on 11/15/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DummyViewController : UIViewController
+
+@end
---
+++ BlickBee/DummyViewController.m
@@ -0,0 +1,37 @@
+//
+//  DummyViewController.m
+//  
+//
+//  Created by Sanchit Kumar Singh on 11/15/15.
+//
+//
+
+#import "DummyViewController.h"
+
+@interface DummyViewController ()
+
+@end
+
+@implementation DummyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
