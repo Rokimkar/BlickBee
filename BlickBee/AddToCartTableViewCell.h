313bab40ee52be569b65f4b53a822d2aaac029bf
Sanchit Kumar Singh <Rokimkar@Sanchits-MacBook-Pro.local>
2015-11-16 02:43:56 +0530

    cover cell Done auto layout pending

---
+++ BlickBee/AddToCartTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  AddToCartTableViewCell.h
+//  BlickBee
+//
+//  Created by Sanchit Kumar Singh on 11/15/15.
+//  Copyright © 2015 Sanchit Kumar Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddToCartTableViewCell : UITableViewCell
+
+@end
---
+++ BlickBee/AddToCartTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  AddToCartTableViewCell.m
+//  BlickBee
+//
+//  Created by Sanchit Kumar Singh on 11/15/15.
+//  Copyright © 2015 Sanchit Kumar Singh. All rights reserved.
+//
+
+#import "AddToCartTableViewCell.h"
+
+@implementation AddToCartTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
